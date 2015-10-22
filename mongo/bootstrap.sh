20d5eb942f6843f05b0016ab005436c98d302bf4
Andy Walker <walkeraj@gmail.com>
2015-10-22 17:44:47 -0400

    sed substitution was a little more zealous than expected

--- mongo/bootstrap.sh
+++ mongo/bootstrap.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get -y install mongodb
 sed -i 's/^bind_ip.*/bind_ip = 0.0.0.0/' /etc/mongodb.conf
-sed -ie 's|#.*\(port.*\)|\1|' /etc/mongodb.conf
+sed -ie 's|# *\(port.*\)|\1|' /etc/mongodb.conf
 service mongodb restart
